05fc6d8f799de6b05f07a62c612c83874a940e0d
Wisnu Adi Nurcahyo <wisn.adn@gmail.com>
2018-03-15 23:33:15 +0700

    Fix register page

--- public/js/application.js
+++ public/js/application.js
@@ -3,6 +3,15 @@
  * @copyright   Bukube Development Team, 2018
  */
 
+const eq = (p, q) => p === q;
+
+const or = (...conds) =>
+  conds.length < 1
+    ? false
+    : !!conds[0]
+      ? true
+      : or(...conds.slice(1));
+
 const getPage = name => {
   const elm = document.getElementsByClassName(name +'-page');
 
@@ -60,16 +69,18 @@ const loginActions = () => {
         const identifier = form.querySelector('input[name=identifier]').value;
         const password = form.querySelector('input[name=password]').value;
 
-        if (identifier.length < 1 || password.length < 1) {
+        if (or(identifier.length < 1, password.length < 1)) {
           show(msg, 'Ada masukan form yang masih kosong.', form);
         } else {
           const user = users
             .filter(user =>
-              user.username === identifier || user.email === identifier
+              or(user.username === identifier, user.email === identifier)
             );
 
           if (user.length > 0 && user[0].password === password) {
             show(msg, 'Mengalihkan ke halaman utama...', form);
+
+            Array.from(inputs).map(elm => elm.disabled = true);
           } else {
             show(msg, 'Informasi login salah.', form);
           }
@@ -103,31 +114,54 @@ const registerActions = () => {
       Array.from(inputs).map(elm => elm.disabled = true);
       form.querySelector('input[type=submit]').value = 'Mencoba Registrasi...';
 
-      const msg = pages.login.querySelector('.alert');
+      const msg = pages.register.querySelector('.alert');
       msg.innerText = '';
 
-      // const users = getJSON('/json/users.json');
-      /*if (!!users) {
-        const identifier = form.querySelector('input[name=identifier]').value;
+      const users = getJSON('/json/users.json');
+      if (!!users) {
+        const email = form.querySelector('input[name=email]').value;
+        const username = form.querySelector('input[name=username]').value;
+        const fullname = form.querySelector('input[name=fullname]').value;
         const password = form.querySelector('input[name=password]').value;
 
-        if (identifier.length < 1 || password.length < 1) {
+        const conds = [
+          email.length < 1,
+          username.length < 1,
+          fullname.length < 1,
+          password.length < 1,
+        ];
+
+        if (or(...conds)) {
           show(msg, 'Ada masukan form yang masih kosong.', form);
         } else {
           const user = users
             .filter(user =>
-              user.username === identifier || user.email === identifier
+              user.username === username || user.email === email
             );
 
-          if (user.length > 0 && user[0].password === password) {
-            show(msg, 'Mengalihkan ke halaman utama...', form);
-          } else {
-            show(msg, 'Informasi login salah.', form);
-          }
+            if (user.filter(x => x.username === username).length > 0) {
+              show(msg, 'Nama Pengguna tidak tersedia.', form);
+            } else if (user.filter(x => x.email === email).length > 0) {
+              show(msg, 'Alamat Email sudah digunakan.', form);
+            } else {
+              if (fullname.length < 3) {
+                show(msg, 'Panjang Nama Lengkap minimal tiga huruf.', form);
+              } else if (password.length < 5) {
+                show(msg, 'Panjang Kata Kunci minimal lima karakter.', form);
+              } else {
+                show(
+                  msg,
+                  'Akun didaftarkan. Mengalihkan ke halaman akun...',
+                  form,
+                );
+
+                Array.from(inputs).map(elm => elm.disabled = true);
+              }
+            }
         }
       } else {
         show(msg, 'Terjadi kesalahan saat mengambil data dari server.', form);
-      }*/
+      }
 
       return false;
     });
